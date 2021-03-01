d65de43ba76a893d44825da568a1e8884f398366
Monika Gupta <monikagupta94159@gmail.com>
2021-03-01 13:10:07 +0530

    use-effect-basic-example

--- src/useEffect/useEffectBasics.js
+++ src/useEffect/useEffectBasics.js
@@ -2,10 +2,23 @@ import React, { useState, useEffect } from "react";
 
 const UseEffectBasic = () => {
   const [value, setValue] = useState(0);
+
+  // hooks can't be inside condition will throw error
+
+  // if (value > 0) {
+  //   useEffect(() => {
+  //     console.log("effect world");
+  //     document.title = `newMessage (${value})`;
+  //   });
+  // }.
+
   useEffect(() => {
     console.log("effect world");
-    document.title = `newMessage (${value})`;
+    if (value >= 1) {
+      document.title = `newMessage (${value})`;
+    }
   });
+
   console.log("hello world");
   return (
     <div>
